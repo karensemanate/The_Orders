fce547253b369333265457582390ced69fe27907
karensemanate <karen.semanate@soy.sena.edu.co>
2024-10-18 11:08:47 -0600

    creación de los modelos de las apps

--- apps/cocina/models.py
+++ apps/cocina/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.orden.models import Pedido
 
 # Create your models here.
+
+class PedidoCocina(models.Model):
+    pedido = models.OneToOneField(Pedido, on_delete=models.SET_NULL)
+    estado = models.CharField(max_length=20, choices=Pedido.ESTADO, default='E')
+
+
+    def __str__(self):
+        return f"el dedido en la cocina de {self.pedido.id}"
